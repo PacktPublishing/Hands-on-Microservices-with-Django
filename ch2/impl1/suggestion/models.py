86de963ea8c5a370d40964747f9f659d7b0d1eb4
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-11-13 15:05:34 +0100

    Update models.py

--- ch2/impl1/suggestion/models.py
+++ ch2/impl1/suggestion/models.py
@@ -1,6 +1,1 @@
-from django.db import models
-
-
-class LogItem(models.Model):
-    message = models.TextField()
-    system = models.CharField(max_length=20, default="suggestion")
+# from django.db import models
