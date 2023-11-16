6ba8444b00e91e671f6c8bc083b3c54d02d4938b
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-11-16 15:38:24 +0100

    Update urls.py

--- ch2/impl2/suggestion/urls.py
+++ ch2/impl2/suggestion/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from suggestion.views import SuggestionFormView, SuccessView
 
-app_name = "feedback"
+app_name = "suggestion"
 
 urlpatterns = [
-    path("", SuggestionFormView.as_view(), name="feedback"),
+    path("suggestion/", SuggestionFormView.as_view(), name="suggestion"),
     path("success/", SuccessView.as_view(), name="success"),
 ]
