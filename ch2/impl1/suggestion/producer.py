49d298a9c290a78aeac20713f859f4e529956887
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-11-17 08:40:10 +0100

    Update producer.py

--- ch2/impl1/suggestion/producer.py
+++ ch2/impl1/suggestion/producer.py
@@ -8,9 +8,9 @@ channel = connection.channel()
 channel.queue_declare(queue='mail_queue', durable=True)
 
 
-def send_email_message(message):
+def send_email_task_message(task_message):
     channel.basic_publish(exchange='',
                           routing_key='mail_queue',
-                          body=json.dumps(message),
+                          body=json.dumps(task_message),
                           properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE)
                           )
