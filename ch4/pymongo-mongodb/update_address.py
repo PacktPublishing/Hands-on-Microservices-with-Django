c61a0b31cbe5e4b49b18ea85c98cb02dadd6a202
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-11-30 10:31:30 +0100

    Update update_address.py

--- ch4/pymongo-mongodb/update_address.py
+++ ch4/pymongo-mongodb/update_address.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://django-microservice:Ue4fK0VuKavclfbo@cluster0.yjchpyg.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://django-microservice:<password>@<cluster>/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["Subscription"]
 col = db["subscription_address"]
