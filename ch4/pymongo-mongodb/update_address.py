ef418400d5fb5ef29466d7ff2319bed5b9c0c570
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-11-30 10:27:22 +0100

    Add files via upload

---
+++ ch4/pymongo-mongodb/update_address.py
@@ -0,0 +1,12 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://django-microservice:Ue4fK0VuKavclfbo@cluster0.yjchpyg.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["Subscription"]
+col = db["subscription_address"]
+
+col.update_one(
+    {"name": "Monthy Python"},
+    {"$set": {"address": "Liverpool Street"}}
+)
