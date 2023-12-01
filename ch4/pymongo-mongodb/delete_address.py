18721a2eccb7afa80d2cb9361b0fb1c8f85597c9
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-12-01 10:30:54 +0100

    Update delete_address.py

--- ch4/pymongo-mongodb/delete_address.py
+++ ch4/pymongo-mongodb/delete_address.py
@@ -1,8 +1,8 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://django-microservice:<password>@<cluster>/?retryWrites=true&w=majority"
-client = MongoClient(uri, server_api=ServerApi('1'))
+con = "mongodb+srv://django-microservice:<password>@<cluster>/?retryWrites=true&w=majority"
+client = MongoClient(con, server_api=ServerApi('1'))
 db = client["Subscription"]
 col = db["subscription_address"]
 
