840dc2582770f48702cf99a30cc2bc7127faab31
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2023-12-18 11:14:46 +0100

    Create models.py

---
+++ ch5/subscription_apis/address_api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=120, blank=True)
+    address = models.CharField(max_length=120, blank=True)
+    postalcode = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=120, blank=True)
+    country = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(blank=True)
