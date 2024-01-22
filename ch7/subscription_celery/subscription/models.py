c66e8c16b4990c9ff278ecd42360fe6a5e194044
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2024-01-22 10:06:33 +0100

    Create models.py

---
+++ ch7/subscription_celery/subscription/models.py
@@ -0,0 +1,11 @@
+from djongo import models
+
+
+class Address(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120, blank=True)
+    address = models.CharField(max_length=120, blank=True)
+    postalcode = models.CharField(max_length=15, blank=True)
+    city = models.CharField(max_length=120, blank=True)
+    country = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(blank=True)
