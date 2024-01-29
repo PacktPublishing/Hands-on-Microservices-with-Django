d77a93973104b98ba76c0a054fc0a9bf899fdb06
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2024-01-29 13:54:22 +0100

    Update deployment.sh

--- subscription_celery/deployment.sh
+++ subscription_celery/deployment.sh
@@ -5,22 +5,27 @@
 # Run this script:
 # $ ./deployment.sh
 
-# Deploy Django subscription app, Redis, Celery workers, and RESTful API
+# Deploy Django subscription app, Redis, and Celery workers
 
+# Stopping containers 
 docker stop subscription_app 
-docker stop address-api 
 docker stop celery_worker 
 docker stop redis_celery 
-
+# Removing containers 
 docker rm subscription_app 
-docker rm address-api 
 docker rm celery_worker 
 docker rm redis_celery 
-
+# Removing images 
 docker rmi subscription_celery-web 
-docker rmi address-api 
 docker rmi subscription_celery-worker 
-
+# Recreate images and containers 
 docker compose up -d 
+
+# Deploy RESTful API
+docker stop address-api 
+docker rm address-api 
+docker rmi address-api-img
+cd ..
+cd subscription_apis
 docker build . -t address-api-img
-docker run --name address-api --network subscription_celery_default -d address-api-img
+docker run --name address-api --network subscription_celery_default -d -p 7000:7000 address-api-img
