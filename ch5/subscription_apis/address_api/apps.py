182f8f5687b4928aabc54af556248518ff922ec2
WoldmanTech <144144184+WoldmanTech@users.noreply.github.com>
2024-03-13 13:13:19 +0100

    Add files via upload

---
+++ ch5/subscription_apis/address_api/apps.py
@@ -0,0 +1,27 @@
+import os
+import redis
+
+from django.apps import AppConfig
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+class AddressApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'address_api'
+
+    def ready(self) -> None:
+        if os.environ.get('RUN_MAIN'):
+            redis_client = redis.Redis(host='redis', port=6379)
+
+            con = "mongodb+srv://django-microservice:Ue4fK0VuKavclfbo@cluster0.yjchpyg.mongodb.net/?retryWrites=true&w=majority"
+            client = MongoClient(con, server_api=ServerApi('1'))
+            db = client["Subscription"]
+            address_col = db["address_api_address"]
+
+            redis_client.delete('addresses')
+
+            for address in address_col.find():
+                redis_client.lpush('addresses', address["address"])
+
+            print('Addresses added to Redis cache')
